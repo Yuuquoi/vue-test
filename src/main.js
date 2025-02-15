a5eddec5b4667060675a1f965214499809e49925
Yuuquoi <hjing378@gmail.com>
2025-02-16 01:11:10 +0800

    vue material init & use

--- src/main.js
+++ src/main.js
@@ -1,12 +1,26 @@
+// ref: https://vuetifyjs.com/zh-Hans/getting-started/installation/#section-5b8988c5
+// $ npm install vuetify
 import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+// vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify/lib/framework.mjs'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+
+// vuetify
+const vuetify = createVuetify({
+    components, directives
+})
+app.use(vuetify)
 
 app.use(createPinia())
 app.use(router)
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,5 +2,27 @@
 </script>
 
 <template>
-  <p>Hello World</p>
+  <!-- 直接使用程式碼即可產生畫面 -->
+  <v-app id="inspire">
+
+    <v-navigation-drawer
+      class="pt-4"
+      color="grey-lighten-3"
+      model-value
+      rail>
+
+      <v-avatar
+        v-for="n in 6"
+        :key="n"
+        :color="`grey-${n === 1 ? 'darken' : 'lighten'}-1`"
+        :size="n === 1 ? 36 : 20"
+        class="d-block text-center mx-auto mb-9">
+      </v-avatar>
+
+    </v-navigation-drawer>
+
+    <v-main>
+    </v-main>
+
+  </v-app>
 </template>
